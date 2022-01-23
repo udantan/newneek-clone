24c7e2ea87dbce5378b698a6f657dfeb4d774450
박지영 <asa855@nate.com>
2022-01-23 22:16:20 +0900

    contact textfield 모듈화 작업

--- src/screens/Help.js
+++ src/screens/Help.js
@@ -1,8 +1,37 @@
 import React, { useState } from "react";
-import faqDB from "../utils/faqDB";
+import {DESC, DESC_UNSUBSCRIBE, DESC_NOARRIVAL} from "../utils/faqDB";
+import TextFieldStyle from "../utils/TextField.module.css";
 
 export default function Help() {
-    console.log(Object.create(faqDB));
+    const [topicDesc, setTopicDesc] = useState(DESC);
+
+    function selectTopic(event){
+        switch (event.target.value){
+            case "unsubscribe" : setTopicDesc(DESC_UNSUBSCRIBE);break;
+            case "noarrival" : setTopicDesc(DESC_NOARRIVAL);
+            break;
+            case "refund" : setTopicDesc(<TextField/>);
+            break;
+            case "branded" : setTopicDesc(<TextField/>);
+            break;
+            case "etc" : 
+            setTopicDesc(<TextField/>);
+            break;
+        }
+    }
+
+    function TextField(){ return(
+        <div className={TextFieldStyle.textfield}>
+            <form>
+            <input name="email" type="text" className={TextFieldStyle.textfieldinput} placeholder="뉴스레터를 구독하거나 회원가입을 한 이메일을 적어주세요."/>
+            <textarea className={`${TextFieldStyle.textfieldinput} ${TextFieldStyle.desc}`} placeholder="문의 내용을 적어주세요."></textarea>
+            </form>
+            <p className={TextFieldStyle.privacytitle}>개인정보 수집·이용에 대한 안내</p>
+            <p className={TextFieldStyle.privacydesc}>문의를 접수하고 회신하기 위해 최소한의 개인정보(이메일과 문의내용)를 수집하며 후속조치를 위해 1년간 보관합니다. 개인정보 수집·이용을 거부할 수 있으며, 이 경우 답변이 제한됩니다. 더 자세한 내용에 대해서는 개인정보처리방침을 참고하시기 바랍니다.</p>
+            <button className={TextFieldStyle.contactbtn}>문의하기</button>
+        </div>)
+    }
+
   return (
       <div className="help">
         <h1 className="help title">고객센터를 통해 <br></br>궁금증을 해결하세요.</h1>
@@ -44,7 +73,7 @@ export default function Help() {
             <h2 className="faq-contact-title">해결이 안 된 문제가 있나요?</h2>
             <p className="faq-contact-description">문의내용을 남겨주세요. 영업일 기준 5일 내에 입력해주신 이메일로 답변 드릴게요!<br></br> (단, 일방적인 비방에는 답변을 드리지 않을 수 있어요.)</p>
             <p className="faq-contact-description">🦔고슴이: 뉴닉은 일월화수목 근무하슴!</p>
-            <select name ="topic" id="topic-select">
+            <select name ="topic" id="topic-select" onChange={selectTopic}>
                 <option value="" disabled selected>주제를 선택해주세요.</option>
                 <option value="unsubscribe">뉴스레터 구독 해지해주세요.</option>
                 <option value="noarrival">뉴스레터가 안 와요.</option>
@@ -52,7 +81,7 @@ export default function Help() {
                 <option value="branded">뉴닉 뉴스레터에 광고 또는 제휴를 하고 싶어요</option>
                 <option value="etc">기타</option>
             </select>
-            <p className="faq-contact-description">문의하기 전에 자주 묻는 질문들을 빠르게 훑어보세요!</p>
+            <p className="faq-contact-description">{topicDesc}</p>
         </section>
       </div>
   );
